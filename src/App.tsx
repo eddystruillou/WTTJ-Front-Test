af17821ec9c433f0e9075b3f3c9954cd13dbd61b
eddystruillou <eddy.s25@hotmail.fr>
2022-05-07 22:26:40 +0200

    Add call to the welcomekit api to get jobs data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import JobList from './Components/JobsList/JobList';
 
-function App() {
+const App:React.FC = () => {
+
+  const [error, setError] = useState<any>(null);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [jobs, setJobs] = useState<[]>([]);
+
+  useEffect(() => {
+    fetch(`https://www.welcomekit.co/api/v1/embed?organization_reference=Pg4eV6k`)
+      .then(res => res.json())
+      .then(({ jobs }) => {
+          setJobs(jobs);
+          setIsLoaded(true);
+        },
+        error => {
+          setError(error);
+          setIsLoaded(true);
+        }
+      )
+  }, [])
+
   return (
     <div className="App">
       <JobList />
