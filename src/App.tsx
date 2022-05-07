367dbb14c23ed5ccda10940b760d56327d76681d
eddystruillou <eddy.s25@hotmail.fr>
2022-05-07 08:17:51 +0200

    Add styles to the main container (App)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Button } from '@welcome-ui/button';
 
 function App() {
   return (
     <div className="App">
-      <Button>Primary</Button>
     </div>
   );
 }
